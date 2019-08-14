5126c12350225c17f8f0d7c37a79c6babb8bc9ae
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-08-13 21:46:40 -0300

    adicionando mais elementos na tela de conversas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  conversas = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
+
   constructor() {}
 
 }
