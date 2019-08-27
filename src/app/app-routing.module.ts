2c573cc72e7246ee84cfef22172da0b1823b16d4
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-08-26 21:36:55 -0300

    adicionando paginas para trabalhos furturos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Tab1PageModule } from './tab1/tab1.module';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
+  },
+  { path: 'pesquisa', loadChildren: './pesquisa/pesquisa.module#PesquisaPageModule' },
+  { path: 'contatos', loadChildren: './contatos/contatos.module#ContatosPageModule' }
 ];
 @NgModule({
   imports: [
