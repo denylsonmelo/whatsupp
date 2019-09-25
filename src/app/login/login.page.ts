0ab07cb1e2b17f63334d78ac26a74f8158cb36fe
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-09-24 21:26:22 -0300

    falando um pouco mais sobre os metodos de autenticacao do firebase

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,20 +4,41 @@ import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  usuario = { email: '', senha: '' };
+  usuarioFirebase;
 
-  usuario = {email: '', senha: ''};
+  logout() {
+    this.afa.auth.signOut();
+  }
+
+  logar() {
+    this.afa.auth.signInWithEmailAndPassword(
+      this.usuario.email,
+      this.usuario.senha
+    )
+    .then(sucesso => {console.log(sucesso);})
+    .catch(erro => {console.log(erro);});
+  }
 
   registrar() {
-    this.auth.auth.createUserWithEmailAndPassword(this.usuario.email, this.usuario.senha);
-    // this.auth.auth.sign;
+    this.afa.auth
+      .createUserWithEmailAndPassword(this.usuario.email, this.usuario.senha)
+      .then(usuarioFirebase => {
+        console.log('deu certo');
+        console.log(usuarioFirebase);
+      })
+      .catch(erro => {
+        console.log('erro');
+        console.log(erro);
+      });
   }
 
-  constructor(private auth: AngularFireAuth) { }
-
-  ngOnInit() {
+  constructor(private afa: AngularFireAuth) {
+    this.usuarioFirebase = this.afa.authState;
   }
 
+  ngOnInit() {}
 }
